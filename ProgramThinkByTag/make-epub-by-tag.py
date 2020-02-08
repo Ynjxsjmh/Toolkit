38169af8b0cbf6b5a315069b363fcae07b068f06
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-02-08 21:31:45 +0800

    [ProgramThinkByTag] Create epub ncx file

--- ProgramThinkByTag/make-epub-by-tag.py
+++ ProgramThinkByTag/make-epub-by-tag.py
@@ -13,16 +13,16 @@ def main() :
     cwd = os.path.dirname(os.path.abspath(sys.argv[0]))
     os.chdir(cwd)
 
-    tags, all_links, all_images = get_all_tags('../../html/tags')
+    tags, all_links, all_images, all_titles = get_all_tags('../../html/tags')
 
-    for tag, links, images in zip(tags, all_links, all_images):
+    for tag, links, images, titles in zip(tags, all_links, all_images, all_titles):
         if not os.path.exists(cwd + '\\result'):
             os.makedirs(cwd + '\\result')
         os.chdir(cwd + '\\result')
 
         print(cwd)
-
-        epub_file = '编程随想 - {}.epub'.format(decode_tag(tag))
+        book_name = '编程随想 - {}'.format(decode_tag(tag))
+        epub_file = '{}.epub'.format(book_name)
         if os.path.exists(epub_file) :
             os.remove(epub_file)
 
@@ -30,10 +30,16 @@ def main() :
 
         zf = ZipFile(epub_file, 'w', ZIP_DEFLATED)
         os.chdir(cwd)
-        
+
         for folder in ['./conf/', '../../html/'] :
             os.chdir(os.path.join(cwd, folder))
             add_folder(zf, '.', None, links, images)
+
+        ncx = create_ncx(book_name, links, titles)
+        with open('program-think.ncx', 'w') as f:
+            f.write(ncx)
+        zf.write('program-think.ncx')
+
         zf.close()
 
         print('\n{} OK'.format(epub_file))
@@ -45,6 +51,7 @@ def get_all_tags(folder):
     tags = []
     all_links = []
     all_images = []
+    all_titles = []
     children = os.listdir(folder)
 
     for name in children:
@@ -59,10 +66,12 @@ def get_all_tags(folder):
 
         links = []
         images = []
+        titles = []
         div = soup.find("div", {"class": "post"})
 
         for h3 in div("h3"):
             for link in h3("a"):
+                titles.append(link.text)
                 link = link.get("href")
                 images.extend(get_all_images(link))
                 link = link.replace('.', '', 1)
@@ -71,10 +80,19 @@ def get_all_tags(folder):
 
         all_links.append(links)
         all_images.append(images)
+        all_titles.append(titles)
+
+        print("----------------")
+        print(titles)
+        print("----------------")
+        print(links)
+        print("----------------")
+        print(images)
+        print("----------------")
 
         print("Collect tag {} Done".format(tag))
 
-    return tags, all_links, all_images
+    return tags, all_links, all_images, all_titles
 
 
 def get_all_images(file):
@@ -145,6 +163,53 @@ def add_folder(zf, folder, count, target_files, target_images) :
         return count
 
 
+def create_ncx(book_name, links, titles):
+    """
+    .ncx 文件是目录文件
+    """
+    head = """
+        <head>
+        <meta name="dtb:uid" content="http://program-think.blogspot.com/2020/01/" />
+        <meta name="dtb:depth" content="4" />
+        <meta name="dtb:totalPageCount" content="0" />
+        <meta name="dtb:maxPageNumber" content="0" />
+        <meta name="dtb:generator" content="Python script by program.think@gmail.com" />
+        <meta name="provider" content="program-think.blogspot.com" />
+        <meta name="right" content="本博客所有的原创文章，作者皆保留版权。转载博文必须包含本声明，保持博文的完整，并以超链接形式注明作者“编程随想”和该博文的原始网址" />
+        </head>
+        """
+
+    navPoint = ""
+    navPoint_template = """
+      <navPoint id="{0}" playOrder="{1}">
+         <navLabel>
+            <text>{2}</text>
+         </navLabel>
+         <content src="{3}"/>
+      </navPoint>\n
+      """
+
+    play_id = 0
+    for link, title in zip(links, titles):
+        navPoint += navPoint_template.format(play_id, play_id, title, link)
+        play_id = play_id + 1
+
+    ncx = """
+       <?xml version="1.0" encoding="UTF-8"?>
+       <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+       <ncx version="2005-1" xmlns="http://www.daisy.org/z3986/2005/ncx/" xml:lang="zh-CN">
+       {0}
+       <docTitle><text>{1}</text></docTitle>
+       <docAuthor><text>编程随想</text></docAuthor>
+       <navMap>
+       {2}
+       </navMap>
+       </ncx>
+       """.format(head, book_name, navPoint)
+
+    return ncx
+
+
 if '__main__' == __name__ :
     try :
         sys.exit(main())
@@ -152,4 +217,3 @@ if '__main__' == __name__ :
         print('Error:\n' + str(err))
         traceback.print_exc()
         sys.exit(1)
-
